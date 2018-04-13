05dfa23451b1613b03c2c05d85ac33d30e52c411
Emilio Gumayagay <emilioramongumayagay@gmail>
2018-04-13 12:14:09 +0800

    Add purchase request number validation

--- src/main/java/com/eprocurement/purchaserequest/PurchaseRequestController.java
+++ src/main/java/com/eprocurement/purchaserequest/PurchaseRequestController.java
@@ -29,6 +29,9 @@ public class PurchaseRequestController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PurchaseRequestRepository purchaseRequestRepository;
+
 	@GetMapping("/new")
 	public String getNewPurchaseRequestForm(Model model) {
 		model.addAttribute("departments", departmentRepository.findAll());
@@ -37,9 +40,14 @@ public class PurchaseRequestController {
 	}
 	
 	@PostMapping("/save")
-	public String savePurchaseRequest(@ModelAttribute PurchaseRequest purchaseRequest) {
-		purchaseRequestService.createNewPurchaseRequest(purchaseRequest);
-		return "redirect:/pr/"+purchaseRequest.getPrNo()+"/details";	
+	public String savePurchaseRequest(@ModelAttribute PurchaseRequest purchaseRequest, Model model) {
+		if(purchaseRequestRepository.existsById(purchaseRequest.getPrNo())){
+			model.addAttribute("errorMsg","Purchase request number already exists");
+			return "purhcaseRequest";
+		}else{
+			purchaseRequestService.createNewPurchaseRequest(purchaseRequest);
+			return "redirect:/pr/"+purchaseRequest.getPrNo()+"/details";
+		}
 	}
 	
 	//display purchase requests
