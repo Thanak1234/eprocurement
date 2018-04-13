dc823e1bfac2b7252471c59a699f41dd1e062328
Emilio Gumayagay <emilioramongumayagay@gmail>
2018-04-13 12:14:23 +0800

    add username validation

--- src/main/java/com/eprocurement/user/UserController.java
+++ src/main/java/com/eprocurement/user/UserController.java
@@ -1,10 +1,13 @@
 package com.eprocurement.user;
 
+import javax.validation.Valid;
+
 import com.eprocurement.department.DepartmentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
@@ -21,30 +24,32 @@ public class UserController {
 
 	@Autowired
 	private DepartmentRepository departmentRepository;
-	
+
 	@GetMapping("/register")
 	public String getRegistrationForm(Model model) {
 		model.addAttribute("user", new User());
 		model.addAttribute("departments", departmentRepository.findAll());
 		return "user";
 	}
-	
+
 	@PostMapping("/user/save")
 	public String registerNewUser(@ModelAttribute User user) {
-		userService.registerUser(user);	
+
+		userService.registerUser(user);
+
 		return "redirect:/user/all";
+
 	}
-	
+
 	@GetMapping("/user/{user}")
 	public String getUpdateUserForm(@PathVariable User user, Model model) {
-		model.addAttribute("user",userRepository.findById(user.getId()).get());
+		model.addAttribute("user", userRepository.findById(user.getId()).get());
 		model.addAttribute("departments", departmentRepository.findAll());
 		return "user";
 	}
-	
+
 	@GetMapping("/user/all")
 	public String getAllUsers() {
 		return "users";
-	}	
+	}
 }
-
