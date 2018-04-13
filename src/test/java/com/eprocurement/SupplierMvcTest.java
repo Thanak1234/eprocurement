441115b8a3ef4cc5dd5b124c44f701a37919eb5b
Emilio Gumayagay <emilioramongumayagay@gmail>
2018-04-13 12:14:41 +0800

    updated supplier mvc test

--- src/test/java/com/eprocurement/SupplierMvcTest.java
+++ src/test/java/com/eprocurement/SupplierMvcTest.java
@@ -8,13 +8,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Optional;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 import com.eprocurement.supplier.Supplier;
 import com.eprocurement.supplier.SupplierController;
 import com.eprocurement.supplier.SupplierRepository;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,64 +28,52 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SupplierController.class)
-@WithMockUser(roles="ADMIN")
+@WithMockUser(roles = "ADMIN")
 public class SupplierMvcTest {
 
 	@Autowired
 	private MockMvc mvc;
-	
+
 	/*
 	 * Mock Dependencies and Setup test
 	 */
-	@MockBean 
-	private SupplierRepository supplierRepository;
-	
 	@MockBean
-	private Supplier supplier;
-	
-	@Before
-	public void setup() {
+	private SupplierRepository supplierRepository;
+
+	//Test get supplier management
+	@Test
+	public void testSupplierManagement() throws Exception {
+		mvc.perform(get("/supplier/all")).andExpect(status().isOk()).andExpect(view().name("suppliers"));
+	}
+
+	//MVC test for get supplier form
+	@Test
+	public void testGetSupplierForm() throws Exception {
+		mvc.perform(get("/supplier/new")).andExpect(status().isOk()).andExpect(view().name("supplier"))
+				.andExpect(model().attributeExists("supplier"));
+	}
+
+	//MVC test for saving new supplier
+	@Test
+	public void testSaveNewSupplier() throws Exception {
+		mvc.perform(post("/supplier/save").with(csrf())).andExpect(status().is3xxRedirection())
+				.andExpect(header().string("Location", "/supplier/all"));
+	}
+
+	//MCV test for get supplier update form
+	@Test
+	public void testGetSupplierUpdateForm() throws Exception {
+
+		Supplier supplier = new Supplier();
+		supplier.setId(1L);
 		supplier.setSupplierName("ABC Company");
 		supplier.setAddress("Bayombong, Nueva Vizcaya");
 		supplier.setContactNumber("N/A");
-	}
-	
-	
-	//Test get supplier management
-	@Test
-	public void testSupplierManagement()throws Exception{
-		mvc.perform(get("/supplier/all"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("suppliers"));
-	}
-	
-	//MVC test for get supplier form
-	@Test
-	public void testGetSupplierForm()throws Exception{
-		mvc.perform(get("/supplier/new"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("supplier"))
-			.andExpect(model().attributeExists("supplier"));	
-	}
-	
-	//MVC test for saving new supplier
-	@Test
-	public void testSaveNewSupplier()throws Exception{
-		mvc.perform(post("/supplier/save").with(csrf()))
-			.andExpect(status().is3xxRedirection())
-			.andExpect(header().string("Location", "/supplier/all"));	
-	}
-	
-	//MCV test for get supplier update form
-	@Test
-	public void testGetSupplierUpdateForm()throws Exception{
-		if(this.supplierRepository.findById(1L).isPresent()){
-			given(supplierRepository.findById(1L).get()).willReturn(supplier);
-		}
-		mvc.perform(get("/supplier/1"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("supplier"))
-		.andExpect(model().attributeExists("supplier"))
-		.andExpect(model().attribute("supplier", equalTo(supplier)));							
+
+		given(this.supplierRepository.findById(1L)).willReturn(Optional.of(supplier));
+
+		mvc.perform(get("/supplier/1")).andExpect(status().isOk()).andExpect(view().name("supplier"))
+				.andExpect(model().attributeExists("supplier"))
+				.andExpect(model().attribute("supplier", equalTo(supplier)));
 	}
 }
